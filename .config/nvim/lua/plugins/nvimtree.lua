2146ce605d7c1c43530821ab2c3a8d77ee95b9ac
Nikita Kapishev <notwhale@mbam1.home.lo>
2022-09-10 14:19:40 +0500

    add nvim config

---
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -0,0 +1,8 @@
+-- NVIM tree
+
+require('nvim-tree').setup({
+  -- Allow using gx
+  disable_netrw = true,
+  hijack_netrw = true,
+  update_cwd = true,
+})
---
+++ .config/nvim/lua/themes.lua
@@ -0,0 +1,12 @@
+-- Themes
+
+-- TokioNight
+-- vim.g.tokyonight_style = 'night'
+-- vim.g.tokyonight_italic_comments = false
+-- vim.cmd [[colorscheme tokyonight]]
+
+-- Material
+-- vim.g.material_style = 'oceanic'
+-- vim.cmd [[colorscheme material]]
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
