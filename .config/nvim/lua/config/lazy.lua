c31163c38e68d065d82ee002a075d8c625526ab8
notwhale <notwhale@gmail.com>
2024-02-05 10:28:55 +0500

    update lazyvim config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -26,8 +26,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  -- install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "gruvbox" } },
+  checker = { enabled = true, notify = false, frequency = 86400 }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
