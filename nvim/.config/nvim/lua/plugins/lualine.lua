3794c20c4a3a388ee149d7848fe46278a350626f
notwhale <notwhale@gmail.com>
2024-02-11 20:49:45 +0500

    add encoding, fileformat, filetype to lualine

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return { -- or you can return new options to override all the defaults
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return {
+        sections = {
+          lualine_y = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+            { "encoding", "fileformat", "filetype" },
+          },
+        },
+      }
+    end,
+  },
+}
