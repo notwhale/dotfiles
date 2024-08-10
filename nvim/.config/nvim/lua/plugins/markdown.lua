17c0faa21a0550abbbc2bd3989b5ad3c17c6c38b
notwhale <notwhale@gmail.com>
2024-08-11 00:26:06 +0500

    update markdown-preview

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,4 @@
 return {
-  -- add symbols-outline
-  {
-    -- "ixru/nvim-markdown",
-    -- cmd = "",
-    -- keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "[N]o [N]eckpain" } },
-  },
-
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
