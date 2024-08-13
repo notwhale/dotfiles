b67b51def572d7581d6b2a7ccee84f12a8eee7cd
Капишев Никита Владимирович <NKapishev@inno.tech>
2024-08-13 12:04:55 +0500

    update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,12 @@
 local wezterm = require("wezterm")
-
+local mux = wezterm.mux
+local action = wezterm.action
 local config = wezterm.config_builder()
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 config = {
 	automatically_reload_config = true,
@@ -17,6 +23,24 @@ config = {
 		right = 2,
 		top = 0,
 		bottom = 0,
+	},
+	native_macos_fullscreen_mode = true,
+}
+
+config.keys = {
+	{
+		key = ",",
+		mods = "CMD",
+		action = action.SpawnCommandInNewTab({
+			cwd = os.getenv("WEZTERM_CONFIG_DIR"),
+			set_environment_variables = {
+				TERM = "screen-256color",
+			},
+			args = {
+				"/opt/homebrew/bin/nvim",
+				os.getenv("WEZTERM_CONFIG_FILE"),
+			},
+		}),
 	},
 }
 
