d36a6031c0d566299c781ffaa9b1278555036786
Капишев Никита Владимирович <NKapishev@inno.tech>
2024-08-26 11:25:34 +0500

    disable autoformat

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.ansible" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "plugins" },
   },
   defaults = {
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -79,3 +79,6 @@ vim.o.scrolloff = 8
 
 -- Markdown
 vim.g.mkdp_browser = "/Applications/Firefox.app/Contents/MacOS/firefox"
+
+-- Prettier
+vim.g.lazyvim_prettier_needs_config = true
