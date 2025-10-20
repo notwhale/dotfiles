e8c715074e131681da26f12539196ea59be3b757
Nikita Kapishev <NKapishev@inno.local>
2025-10-20 08:42:59 +0300

    comment neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,74 +1,74 @@
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    sources = { "filesystem", "buffers", "git_status" },
-    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
-      filtered_items = {
-        visible = false, -- when true, they will just be displayed differently than normal items
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_hidden = false, -- only works on Windows for hidden files/directories
-        hide_by_name = {
-          "git",
-          ".DS_Store",
-          "thumbs.db",
-        },
-        hide_by_pattern = {
-          --"*.meta",
-          --"*/src/*/tsconfig.json",
-        },
-        always_show = { -- remains visible even if other settings would normally hide it
-          ".gitignore",
-          ".env",
-        },
-        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-          --".DS_Store",
-          --"thumbs.db",
-        },
-        never_show_by_pattern = { -- uses glob style patterns
-          --".null-ls_*",
-        },
-      },
-    },
-    window = {
-      mappings = {
-        ["l"] = "open",
-        ["h"] = "close_node",
-        ["<space>"] = "none",
-        ["Y"] = {
-          function(state)
-            local node = state.tree:get_node()
-            local path = node:get_id()
-            vim.fn.setreg("+", path, "c")
-          end,
-          desc = "Copy Path to Clipboard",
-        },
-        ["O"] = {
-          function(state)
-            require("lazy.util").open(state.tree:get_node().path, { system = true })
-          end,
-          desc = "Open with System Application",
-        },
-        ["P"] = { "toggle_preview", config = { use_float = false } },
-      },
-    },
-    default_component_configs = {
-      indent = {
-        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
-      },
-      git_status = {
-        symbols = {
-          unstaged = "󰄱",
-          staged = "󰱒",
-        },
-      },
-    },
-  },
-}
+--return {
+--  "nvim-neo-tree/neo-tree.nvim",
+--  opts = {
+--    sources = { "filesystem", "buffers", "git_status" },
+--    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+--    filesystem = {
+--      bind_to_cwd = false,
+--      follow_current_file = { enabled = true },
+--      use_libuv_file_watcher = true,
+--      filtered_items = {
+--        visible = false, -- when true, they will just be displayed differently than normal items
+--        hide_dotfiles = false,
+--        hide_gitignored = false,
+--        hide_hidden = false, -- only works on Windows for hidden files/directories
+--        hide_by_name = {
+--          "git",
+--          ".DS_Store",
+--          "thumbs.db",
+--        },
+--        hide_by_pattern = {
+--          --"*.meta",
+--          --"*/src/*/tsconfig.json",
+--        },
+--        always_show = { -- remains visible even if other settings would normally hide it
+--          ".gitignore",
+--          ".env",
+--        },
+--        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+--          --".DS_Store",
+--          --"thumbs.db",
+--        },
+--        never_show_by_pattern = { -- uses glob style patterns
+--          --".null-ls_*",
+--        },
+--      },
+--    },
+--    window = {
+--      mappings = {
+--        ["l"] = "open",
+--        ["h"] = "close_node",
+--        ["<space>"] = "none",
+--        ["Y"] = {
+--          function(state)
+--            local node = state.tree:get_node()
+--            local path = node:get_id()
+--            vim.fn.setreg("+", path, "c")
+--          end,
+--          desc = "Copy Path to Clipboard",
+--        },
+--        ["O"] = {
+--          function(state)
+--            require("lazy.util").open(state.tree:get_node().path, { system = true })
+--          end,
+--          desc = "Open with System Application",
+--        },
+--        ["P"] = { "toggle_preview", config = { use_float = false } },
+--      },
+--    },
+--    default_component_configs = {
+--      indent = {
+--        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+--        expander_collapsed = "",
+--        expander_expanded = "",
+--        expander_highlight = "NeoTreeExpander",
+--      },
+--      git_status = {
+--        symbols = {
+--          unstaged = "󰄱",
+--          staged = "󰱒",
+--        },
+--      },
+--    },
+--  },
+--}
